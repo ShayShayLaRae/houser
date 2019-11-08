6ce2aab0ef27fa723c09bbf584493011badf81d5
Shayla Paulsen <shayla.paulsen@gmail.com>
2019-11-08 10:57:02 -0700

    testing again

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import House from './House/House'
+import House from '../House/House'
 export default class Dashboard extends Component{
     render(){
         return(
