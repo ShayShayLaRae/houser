ce982e985ece9c221235ae291c6a9cfc783b79b0
Shayla Paulsen <shayla.paulsen@gmail.com>
2019-11-08 11:56:26 -0700

    added inputs and clearInputsFn

--- src/Components/Wizard/Wizard.js
+++ src/Components/Wizard/Wizard.js
@@ -1,9 +1,52 @@
 import React, {Component} from 'react';
 
 export default class Wizard extends Component{
+    constructor(){
+        super()
+        this.state= {
+            name: '',
+            address: '',
+            city: '',
+            state: '',
+            zip_code: ''
+        };
+        
+        }
+    clearInputs(){
+        this.setState({name: '', address: '', city: '', state: '', zip_code: ''})
+    }
+
     render(){
+        const{name, address, city, state, zip_code} = this.state;
         return(
-            <div>Wizard</div>
+            <div>
+                <input 
+                value={name}
+                placeholder='title'
+                type='text'
+                 />
+                <input  
+                value={address}
+                placeholder='address'
+                type='text'
+                 />
+                <input  
+                value={city}
+                placeholder='city'
+                type='text'
+                 />
+                 <input 
+                 value={state}
+                 placeholder='state'
+                 type='text'
+                 />
+                 <input 
+                 value={zip_code}
+                 placeholder='zip'
+                 type='text'
+                 />
+
+            </div>
         )
     }
 }
