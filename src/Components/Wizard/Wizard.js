6503741312948a557226d8a997235195a2540492
Shayla Paulsen <shayla.paulsen@gmail.com>
2019-11-08 12:15:28 -0700

    onChange handlers for each input

--- src/Components/Wizard/Wizard.js
+++ src/Components/Wizard/Wizard.js
@@ -15,6 +15,21 @@ export default class Wizard extends Component{
     clearInputs(){
         this.setState({name: '', address: '', city: '', state: '', zip_code: ''})
     }
+    nameChangeHandler(event) {
+        this.setState({name: event.target.value})
+    }
+    addressChangeHandler(event) {
+        this.setState({address: event.target.value})
+    }
+    cityChangeHandler(event) {
+        this.setState({city: event.target.value})
+    }
+    stateChangeHandler(event) {
+        this.setState({state: event.target.value})
+    }
+    zip_codeChangeHandler(event) {
+        this.setState({zip_code: event.target.value})
+    }
 
     render(){
         const{name, address, city, state, zip_code} = this.state;
@@ -24,27 +39,37 @@ export default class Wizard extends Component{
                 value={name}
                 placeholder='title'
                 type='text'
+                onChange={e => this.nameChangeHandler(e)}
                  />
                 <input  
                 value={address}
                 placeholder='address'
                 type='text'
+                onChange={e => this.addressChangeHandler(e)}
                  />
                 <input  
                 value={city}
                 placeholder='city'
                 type='text'
+                onChange={e => this.cityChangeHandler(e)}
                  />
                  <input 
                  value={state}
                  placeholder='state'
                  type='text'
+                 onChange={e => this.stateChangeHandler(e)}
                  />
                  <input 
                  value={zip_code}
                  placeholder='zip'
                  type='text'
+                 onChange={e => this.zip_codeChangeHandler(e)}
                  />
+                 <button onClick={event =>{
+                     this.clearInputs()
+                 }}
+                 >Cancel</button>
+                 <button>Complete</button>
 
             </div>
         )
