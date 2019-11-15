0aa662e22a781e9796e121b9f4267692270d084d
Shayla Paulsen <shayla.paulsen@gmail.com>
2019-11-14 21:01:01 -0700

    added a little styling and linked Cancel button to '/'

--- src/Components/Wizard/Wizard.js
+++ src/Components/Wizard/Wizard.js
@@ -1,24 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 
-export default class Wizard extends Component{
-    constructor(){
+export default class Wizard extends Component {
+    constructor() {
         super()
-        this.state= {
+        this.state = {
             name: '',
             address: '',
             city: '',
             state: '',
             zip_code: ''
         };
-        
-        }
 
-    clearInputs(){
-        this.setState({name: '', address: '', city: '', state: '', zip_code: ''})
     }
-    onClick(){
+
+    clearInputs() {
+        this.setState({ name: '', address: '', city: '', state: '', zip_code: '' })
+    }
+    onClick() {
         console.log('sanity check');
         axios.post('http://localhost:4000/api/wizard', {
             name: this.state.name,
@@ -30,62 +31,66 @@ export default class Wizard extends Component{
         this.clearInputs();
     }
     nameChangeHandler(event) {
-        this.setState({name: event.target.value})
+        this.setState({ name: event.target.value })
     }
     addressChangeHandler(event) {
-        this.setState({address: event.target.value})
+        this.setState({ address: event.target.value })
     }
     cityChangeHandler(event) {
-        this.setState({city: event.target.value})
+        this.setState({ city: event.target.value })
     }
     stateChangeHandler(event) {
-        this.setState({state: event.target.value})
+        this.setState({ state: event.target.value })
     }
     zip_codeChangeHandler(event) {
-        this.setState({zip_code: event.target.value})
+        this.setState({ zip_code: event.target.value })
     }
 
-    render(){
-        const{name, address, city, state, zip_code} = this.state;
-        return(
+    render() {
+        const { name, address, city, state, zip_code } = this.state;
+        return (
             <div>
-                <input 
+                <input
                     value={name}
                     placeholder='title'
                     type='text'
                     onChange={e => this.nameChangeHandler(e)}
-                 />
-                <input  
+                />
+                <input
                     value={address}
                     placeholder='address'
                     type='text'
                     onChange={e => this.addressChangeHandler(e)}
-                 />
-                <input  
+                />
+                <input
                     value={city}
                     placeholder='city'
                     type='text'
                     onChange={e => this.cityChangeHandler(e)}
-                 />
-                 <input 
+                />
+                <input
                     value={state}
                     placeholder='state'
                     type='text'
                     onChange={e => this.stateChangeHandler(e)}
-                 />
-                 <input 
+                />
+                <input
                     value={zip_code}
                     placeholder='zip'
                     type='text'
                     onChange={e => this.zip_codeChangeHandler(e)}
-                 />
-                 <button onClick={event =>{
-                     this.clearInputs()
-                 }}>
-                    Cancel
+                />
+
+                <Link to='/'>
+                    <button onClick={event => {
+                        this.clearInputs()
+                    }}>
+                        Cancel
                  </button>
-                 <button onClick={() => this.onClick()}>
-                     Complete
+                </Link>
+
+                <button onClick={() => this.onClick()}>
+                    Complete
                 </button>
 
             </div>
